1f377c381e52b38497e80b6350a59cab14d15472
AlexWelshPie <a.j.welshpiedrahita@gmail.com>
2021-03-16 15:45:14 -0700

    Final update 2021-03-16

--- hello/views.py
+++ hello/views.py
@@ -1,7 +1,17 @@
-import re
 from django.shortcuts import render
+import re
 from django.utils.timezone import datetime
 from django.http import HttpResponse
 
 def home(request):
-    return Http
+    return HttpResponse("Hello, Django!")
+
+def hello_there(request, name):
+    return render(
+        request,
+        'hello/hello_there.html',
+        {
+            'name': name,
+            'date': datetime.now()
+        }
+    )
