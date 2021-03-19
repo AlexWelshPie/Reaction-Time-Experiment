dedfe611f508929b820771a65ba595d91614b35f
Mycheal <mycheallepur@gmail.com>
2021-03-18 22:30:04 -0700

    Added a new app Crispy_forms to style the register page.

--- web_project/settings.py
+++ web_project/settings.py
@@ -33,6 +33,7 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = [
     'users.apps.UsersConfig',
     'patterns.apps.PatternsConfig',
+    'crispy_forms',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -124,3 +125,5 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
 ]
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
