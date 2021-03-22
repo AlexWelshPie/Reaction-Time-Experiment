2487015b493a212053d752cbc6e67f41b5072112
MychealL <2010rrBMW2494>
2021-03-22 16:30:47 -0700

    Added attr~ for Post class Added onclick to pygalexample.html

--- patterns/models.py
+++ patterns/models.py
@@ -3,12 +3,21 @@ from django.contrib.auth.models import User
 
 
 class Post(models.Model):
+    uniqueID        = models.AutoField(blank=True, primary_key=True)
+    participantID   = models.ForeignKey(User, on_delete=models.CASCADE)
+    representation  = models.CharField(max_length=100)
+    numberofvalues  = models.CharField(max_length=100, default="Default value for numofval")
+    repetition      = models.CharField(max_length=100, blank=True, null=True)
+    values          = models.IntegerField(blank=True, null=True)
+    correctanswer   = models.IntegerField(blank=True, null=True)
+    answer          = models.IntegerField(blank=True, null=True)
+    time            = models.IntegerField(blank=True, null=True)
+
     title = models.CharField(max_length=100)
     content = models.TextField()
-    author = models.ForeignKey(User, on_delete = models.CASCADE)
 
     def __str__(self):
-        return self.title
+        return self.uniqueID
 
 class Fruit(models.Model):
     name = models.CharField(max_length=255)
