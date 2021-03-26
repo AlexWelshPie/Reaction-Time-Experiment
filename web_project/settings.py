96b30e76754c604715b6d1141a2ae8d11fbb4560
Mycheal <mycheallepur@gmail.com>
2021-03-26 15:49:00 -0700

    Pages now reference each other. -Does not save last input. Something with CSRF tokens and CORS. Working on a fix.

--- web_project/settings.py
+++ web_project/settings.py
@@ -33,6 +33,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'corsheaders',
     'users.apps.UsersConfig',
     'patterns.apps.PatternsConfig',
     'crispy_forms',
@@ -46,10 +47,13 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
+    'django.middleware.common.CommonMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
+    'corsheaders.middleware.CorsPostCsrfMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
@@ -133,3 +137,42 @@ CRISPY_TEMPLATE_PACK = 'bootstrap4'
 LOGIN_REDIRECT_URL = 'patterns-home'
 LOGIN_URL = 'login'
 django_heroku.settings(locals())
+
+CORS_ALLOW_ALL_ORIGINS = True
+CORS_ALLOW_CREDENTIALS = True
+CORS_REPLACE_HTTPS_REFERER = True
+
+CORS_ALLOW_METHODS = [
+    'DELETE',
+    'GET',
+    'OPTIONS',
+    'PATCH',
+    'POST',
+    'PUT',
+]
+CORS_ALLOW_HEADERS = [
+    'accept',
+    'accept-encoding',
+    'authorization',
+    'content-type',
+    'dnt',
+    'origin',
+    'user-agent',
+    'x-csrftoken',
+    'x-requested-with',
+    'X-CSRFToken',
+]
+CORS_ALLOWED_ORIGINS = [
+    'http://127.0.0.1:8000',
+]
+
+CSRF_TRUSTED_ORIGINS = [
+    '127.0.0.1:8000/patterns/pygalexample',
+    '127.0.0.1:8000/patterns/bubble5',
+    '127.0.0.1:8000/patterns/bubbleChart9',
+    '127.0.0.1:8000/patterns/bubble25',
+    '127.0.0.1:8000/patterns/home',
+    '127.0.0.1:8000/patterns/about',
+    '127.0.0.1:8000/users/login',
+    '127.0.0.1:8000/users/logout',
+]
